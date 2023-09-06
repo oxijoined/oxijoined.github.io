0d9ca4e6fc188dc7285b5c191ae7bd5eee207707
okcu bebra <dev@okcuu.ru>
2023-09-06 16:54:01 +0300

    lanyard

--- js/lanyard.js
+++ js/lanyard.js
@@ -3,5 +3,5 @@ lanyard({
 }).then((response) => {
     document.getElementsByClassName("lanyard-profile-picture")[0].src = `https://cdn.discordapp.com/avatars/${response.discord_user.id}/${response.discord_user.avatar}.png`;
     document.getElementsByClassName("lanyard-username")[0].innerHTML = `${response.discord_user.username}`;
-    document.getElementsByClassName("lanyard-activity")[0].innerHTML = `Playing ${response.activities[0].name} - ${response.activities[0].details}`;
+    document.getElementsByClassName("lanyard-activity")[0].innerHTML = `${response.activities[0].name} - ${response.activities[0].details}`;
 });
